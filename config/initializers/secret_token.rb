b5f7d94cd9726096142acc772f369758165fc6b4
YOUR NAME <gergoric_dean@yahoo.com>
2015-05-04 23:55:15 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Todo111::Application.config.secret_key_base = 'c821b1fe039d40687fb2c2bb3e8707c5ed55d0b5f81c62ec68d5358ce0aaf6f08aa8cde71a90d27ea6b3f8bb4d490985e4c7efaf3d23c2545b6ae371807c984c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Todo111::Application.config.session_store :cookie_store, key: '_todo111_session'
